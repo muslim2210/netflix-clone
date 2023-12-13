352b47a7b9f12f37ef37999a44e275f89f348f71
muslim2210 <bukhori.muslim2210@gmail.com>
2023-12-13 12:06:25 +0700

    fix membuat upcomming, trending, latest movies, remonded

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import "./App.css";
-import HomePages from "./components/HomePages";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/header/Header";
+import HomePages from "./home/HomePages";
 
 function App() {
   return (
     <>
       <Router>
         <Header />
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch></Switch>
+        <Switch>
+          <Route exact path="/" component={HomePages} />
+          {/* <Route path="/singlepage/:id" component={SinglePage} exact /> */}
+        </Switch>
       </Router>
     </>
   );
--- src/components/HomePages.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const HomePages = () => {
-  return <div>HomePages</div>;
-};
-
-export default HomePages;
